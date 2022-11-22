fd89421ae732c06705f8bda89b75206203b139cb
henrisoho <bakingwithmymother@gmail.com>
2022-11-22 10:54:48 -0600

    first draft

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,30 +15,38 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return 'Henri';
 }
 // Remember to call the function to test
-
+console.log('Hello, ', helloName());
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
   // return firstNumber + secondNumber;
-}
+  return firstNumber + secondNumber;
+}// end addNumbers
+
+console.log( '3 + 4 = ', addNumbers( 3, 4 ) )
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
+function multiplyThree(num1, num2, num3 ){
+  // multiply num1, num2, num3
+  return num1 * num2 * num3;
+}// end multiplyThree
 
-}
+console.log( 'five multiplied three times equals ', multiplyThree( 5, 5, 5 ) );
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+  else{
+    return false;
+  }
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -50,15 +58,31 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  //return last item in arrary
+  if ( array.length === 0 ){
+    return 'Undefined';
+  }//end get last
+  else{
+    return array.slice(-1);
+  }// end undefined
 }
+
+console.log( getLast([]) );
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
+  //find out if 'value' populates 'array'
+  for (var array = 0; array == value; array++) {
+        console.log(array);
+        if (array == value) return true;
+    }
+    return false
 }
+
+console.log(find(5, []));
+  
 
 // ----------------------
 // Stretch Goals
@@ -66,24 +90,51 @@ function find( value, array ){
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if( letter === string.charAt(0) ){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll( array ) {
   let sum = 0
   // TODO: loop to add items
+  for (let i = 0; i < array.length; i += 1) {
+    sum += array[i];
+  }
+
   return sum;
 }
+
+console.log(sumAll([1, 5, 16, 12]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+//create a function that adds any positive number to a new array
+const arr1 = [3, 5, 7, -2, 4, -11];
 
+function populateArray(ar){
+  const posArr = ar.filter(num => num > -1);
+    return posArr;
+}//end function
+
+console.log(populateArray(arr1));
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+//A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+
+function nameString(name){
+  let str1 = 'edabit';
+  result = str1.concat
+
+}
